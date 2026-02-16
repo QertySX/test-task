c3108138ba3d5590d478341d583ccdc42d1076e0
art <art@gmail.com>
2026-02-16 17:10:19 +0200

    Написана модель для комментариев

---
+++ comments/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Comment(models.Model):
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="children",
+        on_delete=models.CASCADE
+    )
+
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    homepage = models.URLField(blank=True)
+
+    text = models.TextField()
+
+    image = models.ImageField(
+        upload_to="comments/images/",
+        blank=True,
+        null=True
+    )
+
+    file = models.FileField(
+        upload_to="comments/files/",
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.username} - {self.created_at}"
